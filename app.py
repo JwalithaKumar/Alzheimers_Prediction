d1ddafb5659a9079b98a5fecf03513332a42d737
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2024-07-27 23:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ from tensorflow.keras.preprocessing import image
 classnames = ['Mild_Demented','Moderate_Demented','Non_Demented','Very_Mild_Demented']
 
 # Load the model
+ model = tf.keras.models.load_model('./Alzheimer.h5')
+
+def preprocess_image(image):
+    # Resize image to 224x224
+    image = image.resize((224, 224))
+
+    # Convert to numpy array
+    image_array = np.array(image) / 255.0  # Normalize pixel values
+
+    # Add channel dimension and convert to RGB
+    image_array = np.expand_dims(image_array, axis=-1)
+    image_array = np.repeat(image_array, 3, axis=-1)
+
+    # Add batch dimension
+    image_array = np.expand_dims(image_array, axis=0)
+
+    return image_array
 
 
 # Streamlit app
@@ -15,16 +32,8 @@ def main():
     if uploaded_file is not None:
         img = Image.open(uploaded_file)
         st.image(img,caption='Uploaded Image.',use_column_width=True)
-        model = tf.keras.models.load_model('./Alzheimer.h5')
-        st.write(model.input_shape)
-        img = img.resize((224, 224))
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=-1)
-        img_array = np.repeat(img_array, 3, axis=-1)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.
-        st.write(img_array.shape)
-        prediction = model.predict(img_array)
+        preprocess_image(img)
+        prediction = model.predict(image_array)
         ind = np.argmax(prediction[0])
         st.write("Predicted Class:", classnames[ind])
 
