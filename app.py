96ec9745bdbb39b4d4497ab28546a9897a090f21
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2024-07-27 22:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-
-classnames = ['Mild_Demented', 'Moderate_Demented', 'Non_Demented', 'Very_Mild_Demented']
+classnames = ['Mild_Demented','Moderate_Demented','Non_Demented','Very_Mild_Demented']
 
 # Load the model
-model = tf.keras.models.load_model('Alzheimers.h5')   
-
+model = tf.keras.models.load_model('Alzheimers.h5')
 
 # Preprocessing function
 def preprocess_image(image):
-    # Assuming your model expects images of size 224x224
     image = image.resize((224, 224))
     image_array = np.array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
@@ -20,12 +17,10 @@ def preprocess_image(image):
 # Streamlit app
 def main():
     st.title("Alzheimers Prediction")
-
-    uploaded_file = st.file_uploader("Choose an image...")
+    uploaded_file = st.file_uploader("Upload your Brain MRI ")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)   
-
         image_array = preprocess_image(image)
         prediction = model.predict(image_array)
         ind = np.argmax(prediction[0])
